003cbf68c9222635bf2a0c69244d8e3a70461acf
Samarth Gowda <samarthgowda@Samarths-MBP.fios-router.home>
2021-02-11 14:37:27 -0500

    mathfunctions

---
+++ math.java
@@ -0,0 +1,10 @@
+import static java.lang.Math.sqrt;
+import java.lang.Math;
+public class Demo {
+   public static void main(String[] arg) {
+      double num = 36.99;
+
+      System.out.println("The number is: " + num);
+      System.out.println("The square root of the above number is: " + Math.ceil(num));
+   }
+}
